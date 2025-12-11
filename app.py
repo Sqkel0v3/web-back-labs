574f975d85c4d378ecbe5e512154a0f87ef1a95e
Roman Fomchenko <romashkaololoev@gmail.com>
2025-12-11 20:37:10 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 from rgz import rgz
 
 load_dotenv()
@@ -47,6 +48,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 app.register_blueprint(rgz)
 
 if __name__ == '__main__':
@@ -556,6 +558,7 @@ def index():
                 <li><a href="/lab6">Шестая лабораторная</a></li>
                 <li><a href="/lab7">Седьмая лабораторная</a></li>
                 <li><a href="/lab8">Восьмая лабораторная</a></li>
+                 <li><a href="/lab9">Девятая лабораторная</a></li>
                 <li><a href="/rgz">РГЗ</a></li>
             </ul>
         </nav>
