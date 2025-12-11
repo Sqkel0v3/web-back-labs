da5e17488abe397c037826cf2e5431db2456e2a9
Roman Fomchenko <romashkaololoev@gmail.com>
2025-12-11 19:46:50 +0700

    4задание

--- lab8.py
+++ lab8.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-lab8_bp = Blueprint('lab8', __name__, template_folder='templates')
+lab8 = Blueprint('lab8', __name__, template_folder='templates')
 
-@lab8_bp.route('/')
+@lab8.route('/')
 def index():
     return render_template('lab8/index.html', username="anonymous")
